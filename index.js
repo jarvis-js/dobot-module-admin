a101aad1bd443b5349d42e2432c1b203b4fb7023
Alex Whitman <alex@alexwhitman.com>
2011-11-18 16:24:15 +0000

    Remove bot.Module.load() - no longer required
    - options are now assigned to module rather than passed in

--- index.js
+++ index.js
@@ -1,36 +1,34 @@
 module.exports = function(bot, module) {
 
-	module.load = function() {
-		module.addCommand('load module :name', function(request, name) {
-			bot._loadModule(name, function(error) {
-				if (error) {
-					request.reply = error;
-					bot.respond(request);
-				}
-				else {
-					request.reply = 'Loaded module: ' + name;
-					bot.respond(request);
-				}
-			});
+	module.addCommand('load module :name', function(request, name) {
+		bot._loadModule(name, function(error) {
+			if (error) {
+				request.reply = error;
+				bot.respond(request);
+			}
+			else {
+				request.reply = 'Loaded module: ' + name;
+				bot.respond(request);
+			}
 		});
+	});
 
-		module.addCommand('unload module :name', function(request, name) {
-			bot._unloadModule(name, function(error) {
-				if (error) {
-					request.reply = error;
-					bot.respond(request);
-				}
-				else {
-					request.reply = 'Unloaded module: ' + name;
-					bot.respond(request);
-				}
-			});
+	module.addCommand('unload module :name', function(request, name) {
+		bot._unloadModule(name, function(error) {
+			if (error) {
+				request.reply = error;
+				bot.respond(request);
+			}
+			else {
+				request.reply = 'Unloaded module: ' + name;
+				bot.respond(request);
+			}
 		});
+	});
 
-		module.addCommand('dump', function(request) {
-			request.reply = bot;
-			bot.respond(request);
-		});
-	};
+	module.addCommand('dump', function(request) {
+		request.reply = bot;
+		bot.respond(request);
+	});
 
 };
