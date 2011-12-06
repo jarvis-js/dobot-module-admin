22594c3964aef66761af18c254acc6996a1f591c
Paul Dixon <paul.dixon@mintbridge.co.uk>
2011-12-06 19:22:17 +0000

    adding basic support for install and uninstalling modules

--- index.js
+++ index.js
@@ -1,4 +1,22 @@
+var npm = require("npm");
+
 module.exports = function(bot, module) {
+
+	module.addCommand('install module :name', function(request, name) {
+		npm.load({}, function (error) {
+			npm.commands.install([name], function (er, data) {
+				bot.emit('module_installed', {name: name, data: data});
+			});
+		});
+	});
+
+	module.addCommand('uninstall module :name', function(request, name) {
+		npm.load({}, function (error) {
+			npm.commands.remove([name], function (er, data) {
+				bot.emit('module_uninstalled', {name: name, data: data});
+			});
+		});
+	});
 
 	module.addCommand('load module :name', function(request, name) {
 		bot._loadModule(name, function(error) {
